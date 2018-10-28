c5e12a2f20c76e427e0b7323f514debf615d2ff2
Pepeco <joao.monteirodemedeiros@gmail.com>
2018-10-27 21:56:19 -0300

    conseguir fazer funcionar com a classe player

--- Main.py
+++ Main.py
@@ -12,25 +12,3 @@ while True:
 
     if game_state == 1:
         game()
-        # window.set_background_color((0, 0, 0))
-        # if keyboard.key_pressed("ESC"):
-        #     game_state = 0
-
-        # player_shoot()
-        # update_counters()
-        # window.set_background_color((0, 0, 0))
-        # # fundo.draw()
-        #
-        # for bullet in bullets:
-        #     bullet.draw()
-        # bullet_movement()
-        #
-        # barricade_1.draw()
-        # barricade_2.draw()
-        # barricade_3.draw()
-        # player.update()
-        # player.movement_update()
-        # window.update()
-
-
-
